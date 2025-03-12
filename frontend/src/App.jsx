c792b2516c75f567023f7a1277ae243c0c63c49e
Sem8 <mssemmi8@gmail.com>
2025-03-12 19:32:26 -0400

    - adds routes and styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
-import './App.css'
+import './css/App.css'
+import {Routes, Route} from 'react-router-dom'
+
 import Home from "./pages/Home"
+import Favorites from './pages/Favorites'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
     <>
-    <Home></Home>
+    <div>
+      <NavBar />
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+      
+    </main>
+    </div>
     </>
-  )
+  );
 }
 
 export default App
